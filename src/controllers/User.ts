9a24f696b41134aa779e919dc44bc1d11db784ae
sivabharathy <gotocva@gmail.com>
2020-02-11 10:05:20 +0530

    Initial push

---
+++ src/controllers/User.ts
@@ -0,0 +1,28 @@
+
+ 
+import { UserDao } from '@daos';
+import { logger } from '@shared';
+import { Request, Response, Router, Express } from 'express';
+import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+import { paramMissingError } from '@shared';
+import { ParamsDictionary } from 'express-serve-static-core';
+
+// Init shared
+const router = Router();
+const userDao = new UserDao();
+
+/******************************************************************************
+ *                      Get All Users - "GET /api/users/all"
+ ******************************************************************************/
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await userDao.getAll();
+        return res.status(OK).json({users});
+    } catch (err) {
+        logger.error(err.message, err);
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        });
+    }
+};
---
+++ src/routes/Users.ts
@@ -0,0 +1,86 @@
+ 
+import { UserDao } from '@daos';
+import { logger } from '@shared';
+import { Request, Response, Router, Express } from 'express';
+import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+import { paramMissingError } from '@shared';
+import { ParamsDictionary } from 'express-serve-static-core';
+
+import { getAllUsers } from "../controllers/User";
+// Init shared
+const router = Router();
+const userDao = new UserDao();
+
+/******************************************************************************
+ *                      Get All Users - "GET /api/users/all"
+ ******************************************************************************/
+
+router.get('/all', getAllUsers);
+
+/******************************************************************************
+ *                       Add One - "POST /api/users/add"
+ ******************************************************************************/
+
+router.post('/add', async (req: Request, res: Response) => {
+    try {
+        const { user } = req.body;
+        if (!user) {
+            return res.status(BAD_REQUEST).json({
+                error: paramMissingError,
+            });
+        }
+        await userDao.add(user);
+        return res.status(CREATED).json({status:200}).end();
+    } catch (err) {
+        logger.error(err.message, err);
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        });
+    }
+});
+
+/******************************************************************************
+ *                       Update - "PUT /api/users/update"
+ ******************************************************************************/
+
+router.put('/update', async (req: Request, res: Response) => {
+    try {
+        const { user } = req.body;
+        if (!user) {
+            return res.status(BAD_REQUEST).json({
+                error: paramMissingError,
+            });
+        }
+        user.id = Number(user.id);
+        await userDao.update(user);
+        return res.status(OK).end();
+    } catch (err) {
+        logger.error(err.message, err);
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        });
+    }
+});
+
+/******************************************************************************
+ *                    Delete - "DELETE /api/users/delete/:id"
+ ******************************************************************************/
+
+router.delete('/delete/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params as ParamsDictionary;
+        await userDao.delete(Number(id));
+        return res.status(OK).end();
+    } catch (err) {
+        logger.error(err.message, err);
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        });
+    }
+});
+
+/******************************************************************************
+ *                                     Export
+ ******************************************************************************/
+
+export default router;
